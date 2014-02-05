488069cf84537c55f38cf8ae0dcce6c131a96841
felesmortis <felesmortis@gmail.com>
2014-02-04 18:50:33 -0800

    Small Changes. Still working on the Skeleton

--- ProjectCamel/src/camel/entity/EntityLiving.java
+++ ProjectCamel/src/camel/entity/EntityLiving.java
@@ -1,11 +1,10 @@
 package camel.entity;
 
 public class EntityLiving extends Entity implements IHealable {
-	protected int str, lck, dex, intel, wis, cha, cum, aff, stn, wtf, fth;
-	protected double mana, stamina, fap ;
-	protected float pth;
-	
-	
+	protected int str/*Strength*/, lck/*Luck*/, dex/*Dexterity*/, intel/*Intelligence*/, wis/*Wisdom*/, cha/*Charisma*/, cum/*Cumliness*/, 
+		stn/*Stance*/, con/*Constitution*/, wtf/*What?*/, fth/*Faith*/, mnd/*Psychic*/, aff/*Magical Affinity-Unchanged*/;
+	protected double mana/*Magical Energy*/, stamina/*Physical Energy*/, fap/*Divine Energy*/, ppt/*Psychic Energy*/;
+	protected float pth/*Pain Threshold*/;
 	public void attack(Entity target, double amount){
 		target.damage(amount);
 	}
@@ -19,5 +18,7 @@ public class EntityLiving extends Entity implements IHealable {
 	public void eat(Entity target, double amount) {
 		hp = Math.min(maxhp, hp + target.consume(amount));
 	}
-	
+	public void move(int direction) {
+		
+	}
 }
